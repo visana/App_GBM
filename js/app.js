1931deab30323561362cf4f50fe244fca3ce0504
visana <46570650+visana@users.noreply.github.com>
2019-02-07 20:06:21 -0200

    Add files via upload

--- js/app.js
+++ js/app.js
@@ -24,3 +24,19 @@ var app = {
         console.log('Received Event: ' + id);
     }*/
 };
+// Add to index.js or the first page that loads with your app.
+// For Intel XDK and please add this to your app.js.
+
+document.addEventListener('deviceready', function () {
+  // Enable to debug issues.
+  // window.plugins.OneSignal.setLogLevel({logLevel: 4, visualLevel: 4});
+  
+  var notificationOpenedCallback = function(jsonData) {
+    console.log('notificationOpenedCallback: ' + JSON.stringify(jsonData));
+  };
+
+  window.plugins.OneSignal
+    .startInit("d14e7a44-ab70-4fba-bef6-09010a66f0f8")
+    .handleNotificationOpened(notificationOpenedCallback)
+    .endInit();
+}, false);
